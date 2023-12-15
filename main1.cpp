9447c2f2f72921ea57af20cc48501a3b1cbae73c
RahmaAlaa <alaarahma779@gmail.com>
2023-12-15 17:55:11 +0200

    remove

--- main1.cpp
+++ main1.cpp
@@ -1,13 +1,11 @@
 #include <iostream>
 using namespace std;
 
-int my_sum(int a , int b ,int c );
-
 int main() {
 	
 	cout << "fridaaaaay" << endl;
 
 	cout << "enter your username " << endl;
 
-	return a+b+c;
+	return 0;
 }
