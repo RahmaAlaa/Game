3a5c81e7c2550c719b210d17c965e0ba192ae51e
RahmaAlaa <alaarahma779@gmail.com>
2023-12-15 17:52:51 +0200

    in new mod

--- main1.cpp
+++ main1.cpp
@@ -1,11 +1,13 @@
 #include <iostream>
 using namespace std;
 
+int my_sum(int a , int b ,int c );
+
 int main() {
 	
 	cout << "fridaaaaay" << endl;
 
 	cout << "enter your username " << endl;
 
-	return 0;
+	return a+b+c;
 }
